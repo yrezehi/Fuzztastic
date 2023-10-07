412cfaebc3b930fa8a0ce516ffc183589586f0c5
unknown <yrezehi@gmail.com>
2023-10-07 06:35:46 -0700

    setup: Sharprompt

---
+++ Fuzztastic/Fuzztastic/Request/HttpRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuzztastic.Request
+{
+    public class HttpRequest
+    {
+
+    }
+}
