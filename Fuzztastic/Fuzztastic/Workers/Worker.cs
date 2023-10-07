18b757e35bc019718c77021d19e925387ba2e17c
unknown <yrezehi@gmail.com>
2023-10-07 07:45:41 -0700

    setup: worker

---
+++ Fuzztastic/Fuzztastic/Workers/Worker.cs
@@ -0,0 +1,39 @@
+﻿namespace Fuzztastic.Workers
+{
+    public class Worker
+    {
+        public int Id { get; set; }
+        public readonly string Description;
+
+        public DateTime StartTime { get; set; }
+        public readonly DateTime CreationTime;
+
+        public Thread Thread { get; set; }
+        public readonly ParameterizedThreadStart Action;
+
+        public Worker(string description, ParameterizedThreadStart action, bool triggerImmediately = false)
+        {
+            CreationTime = DateTime.Now;
+            Description = description;
+            Action = action;
+
+            Thread = new Thread(action);
+            Id = Thread.ManagedThreadId;
+
+            if (triggerImmediately)
+            {
+                StartTime = DateTime.Now;
+                Thread.Start();
+            }
+        }
+
+        public void Trigger()
+        {
+            if (Thread.ThreadState == ThreadState.Unstarted)
+            {
+                StartTime = DateTime.Now;
+                Thread.Start();
+            }
+        }
+    }
+}
