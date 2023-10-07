a17dab57e63787f48f5ffd90d440fda54645c4a0
unknown <yrezehi@gmail.com>
2023-10-07 06:23:01 -0700

    setup: authentication

---
+++ Fuzztastic/Fuzztastic/Authentication/APIKey.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuzztastic.Authentication
+{
+    public class APIKey
+    {
+        private readonly string Value;
+
+        public APIKey(string value) =>
+            Value = value;
+    }
+}
