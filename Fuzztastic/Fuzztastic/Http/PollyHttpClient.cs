ee4d3670533d65c1387eaa2caabeee9bd1b23cbf
unknown <yrezehi@gmail.com>
2023-10-14 00:43:39 -0700

    setup: polly policy

--- Fuzztastic/Fuzztastic/Http/PollyHttpClient.cs
+++ Fuzztastic/Fuzztastic/Http/PollyHttpClient.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Polly;
+using System;
 
 namespace Fuzztastic.Http
 {
-    internal class PollyHttpClient
+    public class PollyHttpClient
     {
+        private readonly static int MAX_RETRY_ATTEMPTS= 3;
+        private readonly static TimeSpan WAIT_BEFORE_RETRY = TimeSpan.FromSeconds(1);
+
+        public PollyHttpClient() =>
+            Policy.Handle<HttpRequestException>()
+                .OrResult<HttpResponseMessage>(response => !response.IsSuccessStatusCode)
+                    .WaitAndRetryAsync(MAX_RETRY_ATTEMPTS, sleepProvider => WAIT_BEFORE_RETRY);
+
+        private void OnRetry(DelegateResult<HttpRequestMessage> result, TimeSpan span, int retryCount, Context context) =>
+            Console.WriteLine($"Retrey: #{retryCount} with reason: {result.Exception.Message}");
     }
 }
