28287ecddcc703a0da80003e0224cd5bf1520fe5
unknown <yrezehi@gmail.com>
2023-10-14 01:02:48 -0700

    setup: ProxySettings

---
+++ Fuzztastic/Fuzztastic/Http/ProxySettings.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+
+namespace Fuzztastic.Http
+{
+    public static class ProxySettings
+    {
+        private readonly static string PROXY_URL = default;
+        private readonly static bool PROXY_BYPASS_ON_LOCAL = default;
+        private readonly static bool PROXY_USE_DEFAULT_CREDENTIALS = default;
+
+        private static WebProxy ProxyInstance =>
+            new()
+            {
+                Address = new Uri(PROXY_URL),
+                BypassProxyOnLocal = PROXY_BYPASS_ON_LOCAL,
+                UseDefaultCredentials = PROXY_USE_DEFAULT_CREDENTIALS,
+            };
+
+        public static HttpClientHandler HttpClientHandlerInstance => 
+            new()
+            {
+                Proxy = ProxyInstance
+            };
+    }
+}
