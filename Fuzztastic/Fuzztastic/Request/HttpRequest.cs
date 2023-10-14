bf7a0d5f8d24bdb20785de7011bed532469a384f
unknown <yrezehi@gmail.com>
2023-10-14 04:12:12 -0700

    edit: builder => httprequest

--- Fuzztastic/Fuzztastic/Request/HttpRequest.cs
+++ Fuzztastic/Fuzztastic/Request/HttpRequest.cs
@@ -1,7 +1,57 @@
-﻿namespace Fuzztastic.Request
+﻿using Newtonsoft.Json;
+using System.Text;
+
+namespace Fuzztastic.Request
 {
     public class HttpRequest
     {
+        private HttpClient HttpClient { get; set; }
+        private string HttpMethod { get; set; }
+        private string URL { get; set; }
+        private StringContent Body { get; set; }
 
+        public static HttpRequest Create() =>
+            new HttpRequest();
+
+        public HttpRequest WithHttpMethod(string method)
+        {
+            HttpMethod = method;
+            return this;
+        }
+
+        public HttpRequest WithUrl(string url)
+        {
+            URL = url;
+            return this;
+        }
+
+        public HttpRequest WithBody(object body)
+        {
+            Body = new StringContent(JsonConvert.SerializeObject(body), Encoding.UTF8, "application/json");
+            return this;
+        }
+
+        public async Task<HttpResponseMessage> Execute()
+        {
+            HttpClient = new HttpClient();
+
+            return HttpMethod switch
+            {
+                "GET" => await GetRequest(),
+                "POST" => await PostRequest(),
+                "PUT" => await PutRequest(),
+                "DELETE" => await DeleteRequest(),
+                _ => throw new ArgumentException($"No HTTP Method found for {HttpMethod}")
+            };
+        }
+
+        private async Task<HttpResponseMessage> GetRequest() =>
+            await HttpClient.GetAsync(URL);
+        private async Task<HttpResponseMessage> PostRequest() =>
+            await HttpClient.PostAsync(URL, Body);
+        private async Task<HttpResponseMessage> PutRequest() =>
+            await HttpClient.PutAsync(URL, Body);
+        private async Task<HttpResponseMessage> DeleteRequest() =>
+            await HttpClient.DeleteAsync(URL);
     }
 }
